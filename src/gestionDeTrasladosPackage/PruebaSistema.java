db86cc79baccd23b7877e050584f7df8ad1dbec5
MatiasTortoriello <matiastortoriello1991@gmail.com>
2025-04-29 14:09:15 -0300

    Creacion de clases

--- src/gestionDeTrasladosPackage/PruebaSistema.java
+++ src/gestionDeTrasladosPackage/PruebaSistema.java
@@ -1,6 +1,6 @@
 package gestionDeTrasladosPackage;
 
-public class GestionDeTraslados {
+public class PruebaSistema {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
