1439f2275b298319857acdb73dad0013221255ea
MatiasTortoriello <matiastortoriello1991@gmail.com>
2025-05-18 18:22:45 -0300

    VehiculoClass

--- src/gestionDeTrasladosDominio/BicicletaClass.java
+++ src/gestionDeTrasladosDominio/BicicletaClass.java
@@ -7,92 +7,20 @@ import gestionDeTrasladosEnums.TipoBicicleta;
 
 public class BicicletaClass extends VehiculoClass{
 
-	private final Integer MAX_CANTIDAD_PAQUETES = 2;
-	private final Double MAX_VOLUMEN_CARGA = 0.125;
-	private final Double MAX_PESO_CARGA = 15.0;
-	private final Integer MAX_CIUDADES_QUE_RECORRE = 1;
+
+	private final Double volumenDeCargaTotal = 0.125;
+	private final Double pesoDeCargaTotal = 15.0;
+	private final Integer ciudadesMaximasQueRecorre = 1;
 	private TipoBicicleta tipoBicicleta;
-	private List<PaqueteClass> listaDePaquetes = new ArrayList<>();
 	
 	BicicletaClass(Double maxVolumenDeCarga, Double maxPesoDeCarga, Integer maxCiudadesQueRecorre, Integer maxCantidadPaquetes, TipoBicicleta tipoBicicleta) {
 		//super(maxVolumenDeCarga, maxPesoDeCarga, maxCiudadesQueRecorre);
-		maxCantidadPaquetes = this.MAX_CANTIDAD_PAQUETES;
-		maxVolumenDeCarga = this.MAX_VOLUMEN_CARGA;
-		maxPesoDeCarga = this.MAX_PESO_CARGA;
-		maxCiudadesQueRecorre = this.MAX_CIUDADES_QUE_RECORRE;
-	}
-	
-	BicicletaClass(Double maxPesoDeCarga, Integer maxCantidadPaquetes){
-		//super( maxPesoDeCarga, maxPesoDeCarga, maxCantidadPaquetes );
-		maxCantidadPaquetes = this.MAX_CANTIDAD_PAQUETES;
-		//maxVolumenDeCarga = this.MAX_VOLUMEN_CARGA;
-		maxPesoDeCarga = this.MAX_PESO_CARGA;
-		//maxCiudadesQueRecorre = this.MAX_CIUDADES_QUE_RECORRE;
 
+		maxVolumenDeCarga = this.volumenDeCargaTotal;
+		maxPesoDeCarga = this.pesoDeCargaTotal;
+		maxCiudadesQueRecorre = this.ciudadesMaximasQueRecorre;
 	}
+	
 
-	
-	public Boolean existePaquete(PaqueteClass paquete) {
-		/*for (PaqueteClass p : listaDePaquetes) {
-			if (p.getidPaquete().equals(paquete.getidPaquete())) {
-				return true;
-			}
-		}
-		*/
-		return false;
-	}
-	
-	public Integer numeroDePaquete(PaqueteClass paquete) {
-		/*System.out.println(paquete.getidPaquete());
-		return paquete.getidPaquete();*/
-		return 0;
-	}
-	
-	@Override
-	public void agregarPaqueteAlEnvio(PaqueteClass paquete) {
-		
-		if (!listaDePaquetes.contains(paquete) && listaDePaquetes.size() < MAX_CANTIDAD_PAQUETES) {
-			listaDePaquetes.add(paquete);
-		}
-	}
-	
-	
-	@Override			
-	public Double calcularVolumenDelEnvioTotal() {
-		/*Double volumenDelEnvioTotal = 0.0;
-		for (int i = 0; i < arrayDePaquetes.length; i++) {
-			if(arrayDePaquetes[i] != null ) {
-				volumenDelEnvioTotal += arrayDePaquetes[i].calcularVolumen();
-			}
-		}
-		return volumenDelEnvioTotal;*/
-	}
-	
-	@Override
-	public Double calcularPesoDelEnvioTotal() {
-		/*Double pesoDelEnvioTotal = 0.0;
-		for (int i = 0; i < arrayDePaquetes.length; i++) {
-			if(arrayDePaquetes[i] != null ) {
-			pesoDelEnvioTotal += arrayDePaquetes[i].getPeso();
-			}
-		}
-		return pesoDelEnvioTotal;*/
-	}
-	
-	@Override
-	void llevarPaquete(Integer maxCiudadesQueRecorre) {
-		/*Double volumenDelPaquete = 0.0;
-		Double pesoDelpaquete =0.0;
-		
-		volumenDelPaquete = calcularVolumenDelEnvioTotal(); 
-		pesoDelpaquete = calcularPesoDelEnvioTotal();
-		
-		if((volumenDelPaquete <= MAX_VOLUMEN_CARGA) && (pesoDelpaquete <= MAX_PESO_CARGA) && (maxCiudadesQueRecorre <= MAX_CIUDADES_QUE_RECORRE)){
-			System.out.println("La Bicicleta llevara el paquete");
-			//return arrayDePaquetes[];
-		}else {
-			System.out.println("La bicileta no llevara el paquete");
-		}	 */
-	}
 
 }
--- src/gestionDeTrasladosInterfaz/GestionDeTrasladoClass.java
+++ src/gestionDeTrasladosInterfaz/GestionDeTrasladoClass.java
@@ -3,12 +3,11 @@ package gestionDeTrasladosInterfaz;
 import java.util.ArrayList;
 import java.util.HashSet;
 
-import gestionDeTrasladosDominio.DestinoClass;
 import gestionDeTrasladosDominio.VehiculoClass;
 import gestionDeTrasladosDominio.ViajeClass;
 
 public class GestionDeTrasladoClass {
-	private Integer numeroDeGestion
+	private String numeroDeGestion;
     private ArrayList<VehiculoClass> listaVehiculos;
     private HashSet<Integer> paquetesAsignados;
     
@@ -27,118 +26,7 @@ public class GestionDeTrasladoClass {
 		this.numeroDeGestion = numeroDeGestion;
 	}
 
-	public Double getCostoMinimo() {
-		return costoMinimo;
-	}
 
-	public void setCostoMinimo(Double costoMinimo) {
-		this.costoMinimo = costoMinimo;
-	}
-
-	public Double getCostoPorKm() {
-		return costoPorKm;
-	}
-
-	public void setCostoPorKm(Double costoPorKm) {
-		this.costoPorKm = costoPorKm;
-	}
 
 	
-
-	/***
-	 * Agrega una nuevo destino
-	 * 
-	 * @param destino
-	 * @return true si se pudo agregar o false en caso contrario Se debera buscar si
-	 *         ya se ha ingresado con anterioridad y contemplar esta situacion
-	 */
-	public boolean agregarDestino(DestinoClass destino) {
-
-		return false;
-
-	}
-
-	
-	/***
-	 * Buscar un destino
-	 * 
-	 * @param codigo del destino buscado
-	 * @return Destino (null si no existe)
-	 */
-	public DestinoClass buscarDestino(Integer codigo) {
-
-		return null;
-	}
-
-	/****
-	 * Agrega un nuevo viaje. Asigna el costo antes de agregarlo
-	 * 
-	 * @param viaje Viaje
-	 * @return true si se pudo agregar o false en caso contrario
-	 */
-	public boolean agregarViaje(ViajeClass viaje) {
-
-		return false;
-	}
-
-	/***
-	 * Calcula el costo del viaje
-	 * 
-	 * @return double
-	 */
-	public double calcularCostoDelViaje(ViajeClass viaje) {
-		return 0;
-	}
-
-	/****
-	 * Calcula el importe total de todos los viajes
-	 * 
-	 * @return importe total
-	 */
-	public double calcularImporteTotal() {
-		return 0;
-	}
-
-	/***
-	 * Calcula el importe promedio de los viajes
-	 * 
-	 * @return importe promedio
-	 */
-	public double calcularImportePromedio() {
-
-		return 0;
-	}
-
-	/***
-	 * Obtener viajes ordenados por costo ascendente
-	 * 
-	 * @return Viaje[]
-	 */
-	public ViajeClass[] obtenerViajesOrdenadosPorCostoAscendente() {
-
-		return null;
-	}
-	
-	/*public double calcularCosto(Double distancia) {
-    return distancia * PRECIOPORKILOMETRO;
-	}*/
-
-	/****
-	 * Devuelve en formato String un resumen de los viajes realizados Ordenados por
-	 * costo de forma ascendente
-	 * 
-	 * @return Listado de las ventas realizadas
-	 */
-	public String toString() {
-		return "";
-	}
-
-	/*
-	 * Debe determinar el viaje de mayor precio y devolver todos los datos del
-	 * destino y el valor del viaje hallado
-	 */
-	public String viajeDeMayorPrecio() {
-
-		return "";
-	}
 }
