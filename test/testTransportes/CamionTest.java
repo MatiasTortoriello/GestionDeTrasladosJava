bd6cfa2307de32555e92e321896b71fad4b0c33e
MatiasTortoriello <matiastortoriello1991@gmail.com>
2025-05-21 15:33:05 -0300

    TestFinales

--- test/testTransportes/CamionTest.java
+++ test/testTransportes/CamionTest.java
@@ -4,11 +4,31 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import gestionDeTrasladosDominio.CamionClass;
+import gestionDeTrasladosEnums.TipoDeAcoplado;
+import gestionDeTrasladosInterfaz.GestionDeTrasladoClass;
+
 class CamionTest {
 
 	@Test
-	void test() {
 
+	void queSePuedanCrearTodosLosTiposDeAcoplado() {
+		
+		 CamionClass tradicional = new CamionClass(TipoDeAcoplado.Tradicional, "AAA111");
+		 CamionClass remolqueArticulado = new CamionClass(TipoDeAcoplado.RemolqueArticulado, "AAB112");
+
+		 
+		 GestionDeTrasladoClass gestion = new GestionDeTrasladoClass();
+		 
+		 gestion.agregarVehiculo(tradicional);
+		 gestion.agregarVehiculo(remolqueArticulado);
+
+		 
+		 
+		 assertEquals(2, gestion.getVehiculos().size());
+	        
 	}
+	
+	
 
 }
